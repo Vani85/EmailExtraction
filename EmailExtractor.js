195326c386f1d4e4a350f157a26940d6c86c9730
Preeti Mishra <preeti.mishra@softwire.com>
2025-01-21 12:45:36 +0000

    Modified the code to read file

--- EmailExtractor.js
+++ EmailExtractor.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
 
 function readInputFile() {
+    var data;
     fs.readFile('test.txt', (err, data) => {
     if (err) throw err;
 
-    console.log(data.toString());
+    //console.log(data.toString());
     });
+     return data;
 }
 
-readInputFile();
+var input =readInputFile();
 
+var counter = 0
+
+for(var i = 0; i < String(input).length; i++) {
+  if ((String(input).substring(i,13) =='@softwire.com')) { counter = counter + 1 }
+}
+console.log(counter);
