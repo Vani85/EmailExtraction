cc9306f634c0a2eefb896a59778a1328ec54f3e7
Preeti Mishra <preeti.mishra@softwire.com>
2025-01-21 15:54:57 +0000

    Added regex code

--- EmailExtractor.js
+++ EmailExtractor.js
@@ -2,19 +2,33 @@ const fs =
 require('fs');
 
 function readInputFile() {
-    fs.readFile('test.txt', 'utf8',(err, data) => {
-    if (err) throw err;
-    console.log(data);
-    });    
     const fileData = fs.readFileSync('test.txt','utf8');
     return fileData;     
 }
 
 var input =readInputFile();
-
 var counter = 0
 
-for(var i = 0; i < String(input).length; i++) {
-  if ((String(input).substring(i,13) =='@softwire.com')) { counter = counter + 1 }
+//Using legacy code
+
+while(input.length>0)
+{
+    if ((input.includes('@softwire.com'))) {     
+    counter = counter + 1 
+    input = input.substring(input.indexOf("@softwire.com") + 13);
+    }
+    else
+    {
+        break;
+    }
 }
-console.log(counter);
+console.log("Legacy code: " +counter); // count: 301 
+
+//Code which uses regex
+const re = new RegExp("[\w*\.\w*]+@\w*[\.\w*]+");
+var regExMatch = [...input.matchAll("\w*\.\w*@softwire.com ")];
+regExMatch.forEach((match)=>{
+    console.log(match[0]);
+})
+console.log(regExMatch.length);// Expected: 261 match
+
